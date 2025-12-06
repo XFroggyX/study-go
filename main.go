d83c9ba74570565c380d3b4f51b56d14900730f7
froggy <froggy.happy.fr@yandex.ru>
2025-12-06 07:45:18 +0300

    feat: add currency scanning and calculation functions

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func scanCurrency() float64 {
+	var currency float64
+	fmt.Print("Enter currency: ")
+	fmt.Scan(&currency)
+	return currency
+}
+
+func calcCurrency(currency float64, inputCurrency string, outputCurrency string) float64 {
+	return 0.0
+}
+
 func main() {
 	const usdToEurRate float64 = 0.85
 	const usdToRubRate float64 = 110.0
