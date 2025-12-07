ded6f8aa017abc8b71551c0df85fb86c0531cc7f
froggy <froggy.happy.fr@yandex.ru>
2025-12-07 06:32:40 +0300

    refactor: streamline user input handling with dedicated functions for currency and amount

--- main.go
+++ main.go
@@ -12,52 +12,65 @@ func convertCurrency(amount float64, inputCurrency string, outputCurrency string
 		return 0.0
 	}
 
-	if inputCurrency == "usd" && outputCurrency == "eur" {
+	switch {
+	case inputCurrency == "usd" && outputCurrency == "eur":
 		return amount * usdToEurRate
-	} else if inputCurrency == "usd" && outputCurrency == "rub" {
+	case inputCurrency == "usd" && outputCurrency == "rub":
 		return amount * usdToRubRate
-	} else if inputCurrency == "eur" && outputCurrency == "usd" {
+	case inputCurrency == "eur" && outputCurrency == "usd":
 		return amount / usdToEurRate
-	} else if inputCurrency == "eur" && outputCurrency == "rub" {
+	case inputCurrency == "eur" && outputCurrency == "rub":
 		return amount * usdToRubRate / usdToEurRate
-	} else if inputCurrency == "rub" && outputCurrency == "usd" {
+	case inputCurrency == "rub" && outputCurrency == "usd":
 		return amount / usdToRubRate
-	} else if inputCurrency == "rub" && outputCurrency == "eur" {
+	case inputCurrency == "rub" && outputCurrency == "eur":
 		return amount / (usdToRubRate * usdToEurRate)
 	}
 
 	return 0.0
 }
 
+func scanCurrency() string {
+	fmt.Print("Введите исходную валюту (usd, eur, rub): ")
+	var inputCurrency string
+	fmt.Scan(&inputCurrency)
+	if inputCurrency != "usd" && inputCurrency != "eur" && inputCurrency != "rub" {
+		fmt.Println("Неверная валюта")
+		return scanCurrency()
+	}
+	return inputCurrency
+}
+
+func scanOutputCurrency() string {
+	fmt.Print("Введите целевую валюту (usd, eur, rub): ")
+	var outputCurrency string
+	fmt.Scan(&outputCurrency)
+	if outputCurrency != "usd" && outputCurrency != "eur" && outputCurrency != "rub" {
+		fmt.Println("Неверная валюта")
+		return scanOutputCurrency()
+	}
+	return outputCurrency
+}
+
+func scanAmount() float64 {
+	fmt.Print("Введите сумму: ")
+	var amount float64
+	fmt.Scan(&amount)
+	if amount < 0 {
+		fmt.Println("Сумма должна быть положительной")
+		return scanAmount()
+	}
+	return amount
+}
+
 func main() {
 	fmt.Println("Добро пожаловать в конвертер валют!")
 
 	for {
-		fmt.Print("Введите исходную валюту (usd, eur, rub): ")
-		var inputCurrency string
-		fmt.Scan(&inputCurrency)
-		if inputCurrency != "usd" && inputCurrency != "eur" && inputCurrency != "rub" {
-			fmt.Println("Неверная валюта")
-			continue
-		}
-		fmt.Print("Введите целевую валюту (usd, eur, rub): ")
-		var outputCurrency string
-		fmt.Scan(&outputCurrency)
-		if outputCurrency != "usd" && outputCurrency != "eur" && outputCurrency != "rub" {
-			fmt.Println("Неверная валюта")
-			continue
-		} else if inputCurrency == outputCurrency {
-			fmt.Println("Исходная и целевая валюты не могут быть одинаковыми")
-			continue
-		}
-		fmt.Print("Введите сумму: ")
-		var amount float64
-		fmt.Scan(&amount)
-		if amount < 0 {
-			fmt.Println("Сумма должна быть положительной")
-			continue
-		}
+		inputCurrency := scanCurrency()
+		outputCurrency := scanOutputCurrency()
+		amount := scanAmount()
 
-		fmt.Println(convertCurrency(amount, inputCurrency, outputCurrency))
+		fmt.Println("result", convertCurrency(amount, inputCurrency, outputCurrency))
 	}
 }
